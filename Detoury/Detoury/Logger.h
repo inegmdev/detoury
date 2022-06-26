735d9b7511fb1e323a3cbf995b958a77c20f841d
Islam NEGM <islam.g.negm@gmail.com>
2022-06-26 23:38:43 +0200

    Implement dynamic filename for log based on time

--- Detoury/Detoury/Logger.h
+++ Detoury/Detoury/Logger.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "spdlog/spdlog.h"
+#include <string>
 
 class Logger {
 private:
 	std::shared_ptr<spdlog::logger> m_logger;
+	std::string m_log_file_name;
 public:
 	Logger();
 	void init();
